c5bf16f26cbd2c6558cce6d270ba48ca01e481df
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-07-29 11:22:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
-package Lambda;
-
+import java.util.function.Consumer;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
-		List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
-		names.stream()
-		     .filter(name -> name.startsWith("A"))
-		     .forEach(name -> System.out.println(name));
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
 
-	}
+        Consumer<String> printConsumer = name -> System.out.println(name);
+        names.forEach(printConsumer);
+    }
 }
